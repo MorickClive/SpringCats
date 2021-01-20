790f5f3f4387579deaebd6b0a980c5b63019879a
MorickClive <MorickClive@Gmail.com>
2021-01-20 09:50:33 +0000

    AD: adding sql.data, removing Cat duplicate model class

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO CAT_DOMAIN (name, age, speech_volume) VALUES 
+('Tesla', 10, 11.5),
+('Boris', 10, 11.5),
+('Petra', 17, 6),
+('Rhubarb', 7, 20),
+('Custard', 5, 6),
+('Tango', 11, 4.5);
